355bb290dc051ce0a1913c852fd14953f1c62898
Abiyyu Daffa Haidar Setiawan <abiyyudaffa69@gmail.com>
2026-06-08 13:41:57 +0700

    chore: scaffold React + Vite project with Vitest and gh-pages

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>For Selvi Rebecca</h1>
+      <p>Coming soon...</p>
+    </div>
+  )
+}
+
+export default App
